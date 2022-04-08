cfbf84fe09ad4b9f1afc750ef622eddf763d2694
vbladex <adamlundblade@gmail.com>
2022-04-08 15:29:30 -0500

    Formatting changes

--- Recursion.Files.CropFinder.py
+++ Recursion.Files.CropFinder.py
@@ -4,28 +4,16 @@ import json
 path = 'C:\\Yield\\AgIntegrated'
 files = []
 for r, d, f in os.walk(path):
-	for file in f:
-		if '.json' in file:
-			files.append(os.path.join(r,file))
-	
+    for file in f:
+        if '.json' in file:
+            files.append(os.path.join(r, file))
+
 for f in files:
-        json.loads(['MonitorName'])
-        if 'Corn' in file:
-            print('Corn')
-        else:
-            print(file,'NotCorn')
-    
-                
+    json.loads(['MonitorName'])
+    if 'Corn' in files:
+        print('Corn')
+    else:
+        print(files, 'NotCorn')
 
-
-
-
-
-
-
-
-
-
-
-    #print(f, file=open('C:\\_Output\\DBF.txt', 'a+'))
-    #https://docs.python.org/3.6/library/pathlib.html#module-pathlib path manipulation stuff
+# print(f, file=open('C:\\_Output\\DBF.txt', 'a+'))
+# https://docs.python.org/3.6/library/pathlib.html#module-pathlib path manipulation stuff
