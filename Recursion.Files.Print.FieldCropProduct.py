a259d08bdbce646470b8b64337e2cfcab6bd6e51
vbladex <adamlundblade@gmail.com>
2022-05-04 12:57:24 -0500

    This needs edited, only a pasted of existing file.

--- Recursion.Files.Print.FieldCropProduct.py
+++ Recursion.Files.Print.FieldCropProduct.py
@@ -1,19 +1,23 @@
 import os
 import json
 
-path = ''C:\\Users\\LundbladeA\\Reed Elsevier Group ICO Reed Elsevier Inc\\OG-ProAM agX - Product - Troubleshooting\\HeartlandSoilSampling\\2022-04-08\\LargeExtentsPatrickQuarter\\EarlyOutQA\\Extract''
+path = 'C:\\Users\\LundbladeA\\Reed Elsevier Group ICO Reed Elsevier Inc\\OG-ProAM agX - Product - Troubleshooting\\ShawnPeterson\\NutrienInnovation\\EO'
 files = []
 for r, d, f in os.walk(path):
     for file in f:
         if '.json' in file:
             files.append(os.path.join(r, file))
 
-for f in files:
-    json.loads(['MonitorName'])
-    if 'Corn' in files:
-        print(files, 'Corn')
-    else:
-        print(files, "NotCorn")
+for x in [files]:
+    with open([files]) as json_file:
+        json.load(json_file)
+
+
+
+    #if 'Corn' in files:
+    #   print(files, 'Corn')
+    #else:
+    #    print(files, 'NotCorn')
 
 # print(f, files=open('C:\\_Output\\DBF.txt', 'a+'))
 # https://docs.python.org/3.6/library/pathlib.html#module-pathlib path manipulation stuff
